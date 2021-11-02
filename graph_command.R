2ab908ea2d1b5a5287a50583b46804470acd67c2
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-11-02 17:24:40 +0000

    Added R  (ggplot2) command that allows the data to be displayed on a graph

---
+++ graph_command.R
@@ -0,0 +1,11 @@
+# command to get a graph of the function times
+# in the CSV file, the function names should be changed to:
+#   t_loadImages = 1
+#   t_calculateMedianHues = 2
+#   t_sortImagesByHue = 3
+# this is so that the graph can be delivered as a line graph as worded (discrete) data cannot be
+
+ggplot(data=times, aes(x=V1, y=V2))
+  + geom_line()
+  + xlab("Functions (1 = t_loadImages, 2 = t_calculateMedianHues, 3 = t_sortImagesByHue)")
+  + ylab("Time (ms)")
