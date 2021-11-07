1effc41c07d57fa4ef6383a8cb00f7ae875ecc7c
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-11-07 11:55:52 +0000

    Slightly improved R file readibility

--- graph_commands.R
+++ graph_commands.R
@@ -7,7 +7,6 @@ library(ggplot2)
 #   t_calculateMedianHues = 2
 #   t_sortImagesByHue = 3
 # this is so that the graph can be delivered as a line graph as worded (discrete) data cannot be
-
 ggplot(data=times, aes(x=V1, y=V2))
   + geom_line()
   + geom_text(aes(label=V2, vjust="outward", hjust="outward"))
@@ -17,7 +16,6 @@ ggplot(data=times, aes(x=V1, y=V2))
 # for the sequential function
 # the only difference is the x-axis label as the sequential function isn't split into separate functions
 # so the three major operations can't be identified by their function name
-
 ggplot(data=times, aes(x=V1, y=V2))
   + geom_line()
   + geom_text(aes(label=V2, vjust="outward", hjust="outward"))
